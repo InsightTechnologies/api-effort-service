bb70665006bac1cb4a6d61f17d9489fcbd8cf3c2
samhithar <samhithaeega@gmail.com>
2019-05-31 15:34:39 -0500

    Updated commit

--- src/main/java/com/miracle/effort/exception/EffortErrorCode.java
+++ src/main/java/com/miracle/effort/exception/EffortErrorCode.java
@@ -1,5 +1,7 @@
 package com.miracle.effort.exception;
 
-public class EffortErrorCode {
-
+public interface EffortErrorCode {
+	String EFFORT_CONTROLLER_UNKNOWN_EXCEPTION = "MSB.EFF.UN.000";
+	String EMPTY_STORY_IDS = "MSB.EFF.STR.000";
+	String EMPTY_STORY_STATES = "MSB.EFF.STE.000";
 }
